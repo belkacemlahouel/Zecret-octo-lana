ce11f3f3b1e97715a8eaaa8c74013ec835015aae
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-06-04 22:44:28 +0200

    On isole le problème du tri std::sort qui foire.

---
+++ Nono.h
@@ -0,0 +1,31 @@
+#ifndef __NONO__
+#define __NONO__
+
+#include <iostream>
+
+using namespace std;
+
+class Nono {
+	private:
+		int num;
+
+	public:
+		Nono(int _num) : num(_num) {};
+
+		~Nono(){};
+
+		int getNum() {
+			return num;
+		};
+
+		void print() {
+			cout << "Nono : " << num << endl;
+		};
+
+		bool operator<(Nono& n) {
+			return getNum() < n.getNum();
+		};
+
+};
+
+#endif // __NONO__
--- test.cpp
+++ test.cpp
@@ -1,85 +1,42 @@
 #include <vector>
-#include <iostream>
 #include <algorithm>
-#include "Produit.h"
-#include "Client.h"
+#include "Nono.h"
 
 using namespace std;
 
-struct tmpfoncteur {
-	bool operator() (Produit& p1, Produit& p2) {
-		return p1.dateDue() < p2.dateDue();
-	}
-};
-
-/*bool compareProduits(Produit& p1, Produit& p2) {
-	return p1.dateDue() < p2.dateDue();
-}*/
-
-bool operator< (Produit& p1, Produit& p2) {
-	cout << "Entrée opérateur <2" << endl;
-	if (p1.dateDue() < p2.dateDue())
-		return true;
-	return false;
-}
-
-/*bool operator() (Produit& p1, Produit& p2) {
-	cout << "Entrée opérateur ()2" << endl;
-	if (p1.dateDue() < p2.dateDue())
-		return true;
-	return false;
-}*/
-
-/*template<class T> class PointerComparator<T> :
-class binary_function<bool, T*, T*> {
-	bool operator()(T* a, T* b) {
-		return *a < *b;
-	}
-}*/
-
 int main(int argc, char** argv) {
-	vector<int> vect;
-	vect.push_back(3);
-	vect.push_back(2);
-	vect.push_back(1);
-	for (int i = 0; i < 3; ++i)
-		cout << vect[i] << " ";
-	cout << endl;
-	sort(vect.begin(), vect.end());
-	for (int i = 0; i < 3; ++i)
-		cout << vect[i] << " ";
 	cout << endl;
 
-	cout << endl;
-	cout << endl;
-
-	Client* clt = new Client(1, 1, 1);
-	vector<Produit*> produits;
-	produits.push_back(new Produit(1, 310, clt));
-	produits.push_back(new Produit(5, 400, clt));
-	produits.push_back(new Produit(2, 310, clt));
-	produits.push_back(new Produit(3, 300, clt));
-	produits.push_back(new Produit(4, 360, clt));
+	vector<Nono*> nonos;
+	nonos.push_back(new Nono(1));
+	nonos.push_back(new Nono(2));
+	nonos.push_back(new Nono(4));
+	nonos.push_back(new Nono(5));
+	nonos.push_back(new Nono(3));
 
 	for (int i = 0; i < 5; ++i)
-		produits[i]->printProduit();
+		nonos[i]->print();
 	cout << endl;
-	// stable_sort(produits.begin(), produits.end(), foncteur);
-	sort(produits.begin(), produits.end(), Produit::compare);
+
+	// --------------------------------
+	sort(nonos.begin(), nonos.end());
+	// --------------------------------
+
 	for (int i = 0; i < 5; ++i)
-		produits[i]->printProduit();
+		nonos[i]->print();
 	cout << endl;
 
 	cout << endl;
 	cout << endl;
 
-	produits[4]->printProduit();
-	if (*produits[4] < *produits[0]) {
+	// Test opérateur seul
+	nonos[4]->print();
+	if (*nonos[4] < *nonos[0]) {
 		cout << " < ";
 	} else {
 		cout << " > ";
 	}
-	produits[0]->printProduit();
+	nonos[0]->print();
 
 	return 0;
 }
