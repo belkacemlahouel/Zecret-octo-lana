fa3a741510f0431f63e81b5778b54917a27ea147
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-07-07 20:46:22 +0200

    Solving instanciation issue (hard creation of Cases)

--- Pogo/Board.cs
+++ Pogo/Board.cs
@@ -9,5 +9,19 @@ public class Board {
 
 	public Board() {
 		cases = new Case[3, 3];
+
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				cases[i, j] = new Case();
+			}
+		}
+
+		for (int i = 0; i < 3; ++i) {
+			cases[0, i].Push(new Pawn(PawnColor.BLACK));
+			cases[0, i].Push(new Pawn(PawnColor.BLACK));
+
+			cases[2, i].Push(new Pawn(PawnColor.WHITE));
+			cases[2, i].Push(new Pawn(PawnColor.WHITE));
+		}
 	}
 }
