eddcb4bae0adf074e8238ea1e71bb398191c9c9b
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-07-07 21:27:18 +0200

    Implementation of a squeletton for the model

--- Pogo/PogoModel.cs
+++ Pogo/PogoModel.cs
@@ -1,5 +1,56 @@
 using System;
 
 public class PogoModel {
+	private Board board;
+	private Player whitePlayer;
+	private Player blackPlayer;
+	private PawnColor turn;
 
+	public Board Board {
+		get {return board;}
+	}
+
+	public Player WhitePlayer {
+		get {return whitePlayer;}
+	}
+
+	public Player BlackPlayer {
+		get {return blackPlayer;}
+	}
+
+	public PawnColor Turn {
+		get {return turn;}
+	}
+
+	public PogoModel() {
+		board = new Board();
+
+		// Creation of both players: default is two "HumanPlayer"
+		whitePlayer = new HumanPlayer(PawnColor.WHITE);
+		blackPlayer = new HumanPlayer(PawnColor.BLACK);
+
+		turn = PawnColor.WHITE;
+
+		run();
+	}
+
+	public PogoModel(bool whitePlayerHuman, bool blackPlayerHuman) {
+		board = new Board();
+
+		if (whitePlayerHuman)
+			whitePlayer = new HumanPlayer(PawnColor.WHITE);
+		// else: creation of an artifical player
+
+		if (blackPlayerHuman)
+			blackPlayer = new HumanPlayer(PawnColor.BLACK);
+		// else: creation of an artificial player
+
+		turn = PawnColor.WHITE;
+
+		run();
+	}
+
+	private void run() {
+
+	}
 }
