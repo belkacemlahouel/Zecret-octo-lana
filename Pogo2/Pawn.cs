d38de6f546fbcfde4f565cb8f00ee7fc67837feb
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-07-12 18:57:30 +0200

    Adding Pawn class + tests inside Main method.

---
+++ Pogo2/Pawn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Pawn {
+    private PawnColor color;
+
+    public PawnColor Color {
+        get { return color; }
+    }
+
+    public Pawn(PawnColor _color) {
+        color = _color;
+    }
+
+    public override string ToString() {
+        if (color == PawnColor.NULL) return "NULL Pawn";
+        else if (color == PawnColor.WHITE) return "WHITE Pawn";
+        else return "BLACK Pawn";
+    }
+}
+
+public enum PawnColor {
+    NULL = -1,
+    WHITE,
+    BLACK
+}
+
--- Pogo2/ZApplication.cs
+++ Pogo2/ZApplication.cs
@@ -4,6 +4,14 @@ public class ZApplication {
     public static void Main(string[] args) {
         Console.WriteLine("Hello World!");
 
+        Pawn pawnN = new Pawn(PawnColor.NULL);
+        Pawn pawnW = new Pawn(PawnColor.WHITE);
+        Pawn pawnB = new Pawn(PawnColor.BLACK);
+
+        Console.WriteLine(pawnN.ToString());
+        Console.WriteLine(pawnW.ToString());
+        Console.WriteLine(pawnB.ToString());
+
         Console.WriteLine("bye World!");
     }
 }
