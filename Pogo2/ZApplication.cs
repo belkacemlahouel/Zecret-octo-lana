e821fce9ad62ca75342d5643350f76dbc575d809
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-07-12 18:47:15 +0200

    Adding class containing Main method: ZApplication.

---
+++ Pogo2/ZApplication.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class ZApplication {
+    public static void Main(string[] args) {
+        Console.WriteLine("Hello World!");
+
+        Console.WriteLine("bye World!");
+    }
+}
+
