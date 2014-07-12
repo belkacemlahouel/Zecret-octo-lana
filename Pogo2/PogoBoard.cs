60b7207c32241cc7a656fa308206f4600a4884de
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-07-12 21:17:09 +0200

    Improving the console view (ToString() methods) + test. Bug spotted + corrected in PogoBoard().move().

--- Pogo2/PogoBoard.cs
+++ Pogo2/PogoBoard.cs
@@ -21,15 +21,27 @@ public class Case {
         return false;
     }
 
-    public void Push(Queue<Pawn> qp) {
+    public void Push(Stack<Pawn> qp) {
         while (qp.Count > 0) {
-            Push(qp.Dequeue());
+            Push(qp.Pop());
         }
+    }
+
+    public Stack<Pawn> Pop(int i) {
+        Stack<Pawn> rep = new Stack<Pawn>();
+//        int borne = Math.Min(i, pawns.Count);
+        int borne = i;
+
+        for (int j = 0; j < borne; ++j) {
+            rep.Push(pawns.Pop());
+        }
+
+        return rep;
     }
 
     // Printing all Pawns in this Case, stack order style (TOP -> BOT)
     public override string ToString() {
-        string rep = ""; // = "Case containing:";
+        string rep = "";
 
         foreach (Pawn p in pawns) {
             rep += p;
@@ -70,8 +82,16 @@ public class PogoBoard {
         }
     }
 
-    public void move() {
+    public void move(Case c1, int i, Case c2) {
+        c2.Push(c1.Pop(i));
+    }
 
+    // Parameters i1 and i2: number of the cases
+    // 1 2 3 | 0,0  0,1  0,2
+    // 4 5 6 | 1,0  1,1  1,2
+    // 7 8 9 | 2,0  2,1  2,2
+    public void move(int i1, int i, int i2) {
+        move(board[(i1-1)/3, (i1-1)%3], i, board[(i2-1)/3, (i2-1)%3]);
     }
 
     public override string ToString() {
